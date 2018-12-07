97c728eee095056d539293c63f7dd97ddba2638c
Eugene Moskvita <eugene.moskvita@postindustria.com>
2018-12-07 14:10:33 +1100

    List

---
+++ listOfCases.js
@@ -0,0 +1,13 @@
+const lowDinning = [
+    '/data/Dinning/dinning_less_10_dollars/acai-brothers-manly-bowls-healthy--0060I00000Y2nZQQAZ',
+    '/data/Dinning/dinning_less_10_dollars/charlie-rabbit-breakfast-lunch-dessert-drinks--0060I00000Y25AuQAJ',
+    '/data/Dinning/dinning_less_10_dollars/five-stars-thaitanic-sydney-thai-lunch-dinner-bottle-water--0060I00000X99OqQAJ',
+    '/data/Dinning/dinning_less_10_dollars/poke-bros-parramatta-large-poke-bowl-bottle-water--0060I00000Y4af7QAB',
+];
+
+const highDinnig = [
+    '/data/Dinning/dinning_more_69_dollars/aquarius-seafood-restaurant-seafood-platter-wine--0060I00000RAJWdQAP',
+    '/data/Dinning/dinning_more_69_dollars/baygarden-restaurant-novotel-brighton-le-sands-all-you-can-eat-seafood-buffet--0060I00000WKPBAQA5',
+    '/data/Dinning/dinning_more_69_dollars/feast-restaurant-sheraton-on-the-park-sydney-all-you-can-eat-seafood-buffet--0060I00000Y2N7NQAV',
+    '/data/Dinning/dinning_more_69_dollars/sailmaker-hyatt-regency-sydney-seafood-feast-wine-darling-harbour-views--0060I00000Y59HVQAZ'
+];
